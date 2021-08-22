f5a64317609658707241a0fcc728a908d71cd064
Fikir <fikirmuluken@gmail.com>
2021-08-22 21:16:36 +0300

    feat:add a function

--- pSniffer.py
+++ pSniffer.py
@@ -2,7 +2,12 @@ import socket
 import struct
 import textwrap
 
-#Unpack ethernet frame
+# Unpack ethernet frame
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+
+# Return properly formmated MAC Address
+def get_mac_addr(bytes_addr):
+    bytes_str = map('{:02X}'.format, bytes_addr)
+    return ':'.join(bytes_str).upper()
